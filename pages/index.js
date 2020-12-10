e5cf9df760e3bacb98b99fea6c0829767e4d346c
mannycolon <colonmanuel7@gmail.com>
2020-12-10 16:31:30 -0500

    Designed layout component to avoid DRY code

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,19 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Layout from '@components/Layout'
 import Card from '@components/Card'
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <Head>
-        <title>Greek Apparatus</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className='flex-1 flex flex-col justify-center items-center py-4 px-6 bg-gray-200'>
-        <Header title="Greek Apparatus!" />
-        <div className='flex'>
-          <Card
-            title='UGNT'
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa diam, vehicula ut ultrices vitae, porta eget libero. Proin blandit lectus eget ipsum scelerisque, id porta velit malesuada. Proin imperdiet sodales orci, vitae auctor sapien dictum congue.'
-          />
-          <Card
-            title='UGNT'
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa diam, vehicula ut ultrices vitae, porta eget libero. Proin blandit lectus eget ipsum scelerisque, id porta velit malesuada. Proin imperdiet sodales orci, vitae auctor sapien dictum congue.'
-          />
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <div className='flex'>
+        <Card
+          title='UGNT'
+          content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa diam, vehicula ut ultrices vitae, porta eget libero. Proin blandit lectus eget ipsum scelerisque, id porta velit malesuada. Proin imperdiet sodales orci, vitae auctor sapien dictum congue.'
+        />
+        <Card
+          title='UGNT'
+          content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa diam, vehicula ut ultrices vitae, porta eget libero. Proin blandit lectus eget ipsum scelerisque, id porta velit malesuada. Proin imperdiet sodales orci, vitae auctor sapien dictum congue.'
+        />
+      </div>
+    </Layout>
   )
 }
