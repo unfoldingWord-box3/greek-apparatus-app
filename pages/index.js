87998d3a96d73ba5d85ee58ebed6ebf0bbbe4970
Richard Mahn <rich.mahn@unfoldingword.org>
2020-12-11 10:33:37 -0500

    Changes for fetch

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Scripture from '@components/Scripture';
+import { core } from 'scripture-resources-rcl';
 
-export default function Home() {
+export default function Home({ usfm }) {
+  const rowHeight = 60;
+  const scriptureHeight = rowHeight * 3;
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <Head>
@@ -11,13 +17,26 @@ export default function Home() {
       </Head>
 
       <main className='flex-1 flex flex-col justify-center items-center py-20'>
-        <Header title="Welcome to my app!" />
-        <p className="text-center leading-normal text-base">
-          Get started by editing <code className='bg-gray-50 rounded p-3 font-menlo'>pages/index.js</code>
-        </p>
+        <pre>{usfm}</pre>
+        <Scripture height={scriptureHeight} />
       </main>
-
       <Footer />
     </div>
   )
+}
+
+Home.getInitialProps = async (ctx) => {
+  const resourceLink = 'unfoldingWord/el-x-koine/ugnt/master/mrk';
+  const reference = {bookId: 'mrk', chapter: 1, verse: 1};
+  const config = {
+    server: 'https://bg.door43.org',
+    cache: {
+      maxAge: 1 * 1 * 1 * 60 * 1000, // override cache to 1 minute
+    },
+  };
+
+  console.log(core.resourceFromResourceLink);
+
+  const json = await core.resourceFromResourceLink({resourceLink, reference, config});
+  return { usfm: JSON.stringify(json, null, 4) }
 }
