29e784717ad9651b58cc5a99c7b29b95670df6f3
Richard Mahn <rich.mahn@unfoldingword.org>
2020-12-11 13:08:03 -0500

    Changes for scripture download

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import { core } from 'scripture-resources-rcl';
+import Layout from '@components/Layout'
+import Card from '@components/Card'
 
-export default function Home({ usfm }) {
-  const rowHeight = 60;
-  const scriptureHeight = rowHeight * 3;
-
+export default function Home() {
   return (
     <Layout>
       <div className='flex'>
@@ -17,23 +14,6 @@ export default function Home({ usfm }) {
           content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa diam, vehicula ut ultrices vitae, porta eget libero. Proin blandit lectus eget ipsum scelerisque, id porta velit malesuada. Proin imperdiet sodales orci, vitae auctor sapien dictum congue.'
         />
       </div>
-      <pre>{usfm}</pre>
     </Layout>
   )
-}
-
-Home.getInitialProps = async (ctx) => {
-  const resourceLink = 'unfoldingWord/el-x-koine/ugnt/master/mrk';
-  const reference = {bookId: 'mrk', chapter: 1, verse: 1};
-  const config = {
-    server: 'https://bg.door43.org',
-    cache: {
-      maxAge: 1 * 1 * 1 * 60 * 1000, // override cache to 1 minute
-    },
-  };
-
-  console.log(core.resourceFromResourceLink);
-
-  const json = await core.resourceFromResourceLink({resourceLink, reference, config});
-  return { usfm: JSON.stringify(json, null, 4) }
 }
--- pages/viewer.js
+++ pages/viewer.js
@@ -1,10 +1,28 @@
 import Layout from '@components/Layout'
 import Viewer from '@components/Viewer';
+import {core} from 'scripture-resources-rcl';
 
-export default function Home() {
+export default function ViewerPage({ usfm }) {
   return (
     <Layout>
-      <Viewer/>
+      <Viewer usfm={usfm} />
     </Layout>
   )
+}
+
+ViewerPage.getInitialProps = async (ctx) => {
+  const config = {
+    server: 'https://bg.door43.org',
+    cache: {
+      maxAge: 1 * 1 * 1 * 60 * 1000, // override cache to 1 minute
+    },
+  };
+  const params = { username: 'unfoldingword', repository: 'el-x-koine_ugnt', path: '42-MRK.usfm', tag: 'master', config };
+  let usfm = '';
+  try {
+    usfm = await core.getFile(params);
+  } catch(e) {
+    console.log(e);
+  }
+  return { usfm }
 }
