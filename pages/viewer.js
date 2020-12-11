222e672ddbb371eb46cf0c7db2f5e776c9ff7ca5
Richard Mahn <rich.mahn@unfoldingword.org>
2020-12-11 14:52:04 -0500

    Removes context from Statis props

--- pages/viewer.js
+++ pages/viewer.js
@@ -2,7 +2,7 @@ import Layout from '@components/Layout'
 import Viewer from '@components/Viewer';
 import {core} from 'scripture-resources-rcl';
 
-export default function ViewerPage({ usfm }) {
+export default function Home({ usfm }) {
   return (
     <Layout>
       <Viewer usfm={usfm} />
@@ -10,14 +10,19 @@ export default function ViewerPage({ usfm }) {
   )
 }
 
-export async function getStaticProps(context) {
-  const config = {
-    server: 'https://git.door43.org',
-    cache: {
-      maxAge: 1 * 1 * 1 * 60 * 1000, // override cache to 1 minute
+export async function getStaticProps() {
+  const params = {
+    username: 'unfoldingword',
+    repository: 'el-x-koine_ugnt',
+    path: '42-MRK.usfmm',
+    tag: 'master', 
+    config: {
+      server: 'https://git.door43.org',
+      cache: {
+        maxAge: 1 * 1 * 1 * 60 * 1000, // override cache to 1 minute
+      },
     },
   };
-  const params = { username: 'unfoldingword', repository: 'el-x-koine_ugnt', path: '42-MRK.usfmm', tag: 'master', config };
   let usfm = '';
   try {
     usfm = await core.getFile(params);
