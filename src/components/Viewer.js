01582893f17406e5c2ac06f88ee65ba52bcc6ce7
ancientTexts-net <zach.pennington@unfoldingWord.org>
2020-12-11 11:06:34 -0600

    Apparatus tagging and display improvements

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -2,6 +2,7 @@ import Card from '@components/Card'
 
 import {Verses} from "scripture-resources-rcl";
 import usfmJS from 'usfm-js';
+import {tokenize} from 'string-punctuation-tokenizer';
 
 import { parseResourceLink, extendProject, resourceFromResourceLink, getResourceManifest } from 'scripture-resources-rcl';
 import { versesFromReferenceIdAndBooks, referenceIdFromReference } from 'scripture-resources-rcl';
@@ -70,6 +71,49 @@ function Viewer ({
 
     return variantObjects;
   };
+
+  Array.prototype.insert = function ( index, item ) {
+    this.splice( index, 0, item );
+  };
+  
+  const tokenizer = (text) => {
+    return tokenize({
+      text: text,
+      greedy: true,
+      normalize: true,
+    });
+  };
+
+  const normalizeString = (string) => {
+    const normalized = tokenizer(string).join(' ').replace('ʼ', '').replace('’', '');
+    return normalized;
+  };
+
+  const tagVariantsInVerseObjects = (currentVerseObjects, filteredVariantObjects) =>
+  {
+      // {"type":"text","text":", "}
+      //currentVerseObjects[1].verseObjects.insert(3, {type: 'text', text: '⸢'});
+
+      if (currentVerseObjects)
+      {
+        filteredVariantObjects.forEach(variant =>{
+          const variantBaseTextSplit = variant.baseText.split(" ").filter(txt => txt.trim().length > 0);
+
+          currentVerseObjects.verseObjects.forEach(vo => {
+            if (normalizeString(vo.text) === normalizeString(variantBaseTextSplit[0]))
+            {
+              vo.text = '⸢' + vo.text;
+            }
+            if (normalizeString(vo.text) === normalizeString(variantBaseTextSplit[variantBaseTextSplit.length-1]))
+            {
+              vo.text = vo.text + '⸣';
+            }
+          });
+        });
+      }
+
+      return currentVerseObjects;
+  };
   
   const verses = Object.keys(chapters[chapterKey]).filter(key => key.match(/^\d+$/)).map(
     (currentVerseKey) => {
@@ -78,36 +122,39 @@ function Viewer ({
 
       const filteredVariantObjects = filterApparatusData(chapterKey, currentVerseKey);
 
+      currentVerseObjects[1] = tagVariantsInVerseObjects(currentVerseObjects[1], filteredVariantObjects);
+
       //const apparatusData = JSON.stringify(filterApparatusData(chapterKey, currentVerseKey), null, 4)
       let apparatusData = <div> {
         filteredVariantObjects.map(
             currentVariantObject => (
               <>
                 <br/>
-                <span className="apparatusBaseText">[{currentVariantObject.baseText}]</span>
-                <br/>
+                <span className="apparatusBaseText">{currentVariantObject.baseText}</span>
                 {
                   currentVariantObject.readings.map(
                     currentReading => (
                       <>
+                        <span className="apparatusTranslationText">{(currentReading.text.trim().length == 0)? '(Omit)' : currentReading.translations.filter(tl => tl.languageId == "ru")[0].text}</span>
                         <span className="apparatusVariantText">{currentReading.text}</span>
-                        <span>-</span>
-                        <span className="apparatusTranslationText">{currentReading.translations.filter(tl => tl.languageId == "ru")[0].text}</span>
-                        {
-                          currentReading.sources.map(
-                            currentSource => {
-                              if (currentSource.title && currentSource.title.length > 0)
-                              {
-                                if (currentSource.textClass == "mod") {
-                                  return <><span className="apparatusSource">({currentSource.title})</span></>
-                                } else
+                          <span className="apparatusSourceContainer tooltip">
+                            <span className="tooltiptext">Sources</span>
+                            {
+                            currentReading.sources.map(
+                              currentSource => {
+                                if (currentSource.title && currentSource.title.length > 0)
                                 {
-                                  return <span className="apparatusSource">{currentSource.title}</span>
+                                  if (currentSource.textClass == "mod") {
+                                    return <span className="apparatusSource">{'(' + currentSource.title + ')'}</span>
+                                  } 
+                                  else {
+                                    return <span className="apparatusSource">{currentSource.title}</span>;
+                                  }
                                 }
-                              }
+                                }
+                              )
                             }
-                            )
-                        }
+                            </span>
                         <br/>
                       </>
                     )
